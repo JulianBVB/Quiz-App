496d3447d26892bfaf5a4682dbfb1a68271dcb05
Julian <JulianStahl@gmx.de>
2021-08-08 21:39:15 +0200

    right/wrong answer

--- script.js
+++ script.js
@@ -45,4 +45,39 @@ let questions = [
         "right_answer": 1
     },
 
+
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('amount-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+
+    }
+}
