6d17d189f479a3da7931ee47a718a8dda9c5e036
Julian <JulianStahl@gmx.de>
2021-08-08 20:47:17 +0200

    add json

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-let questions = ["Wer hat HTML erfunden", "Wie bindet man eine Website in eine Website ein?"];
+let questions = [
+
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Was ist das schönste Dorf?",
+        "answer_1": "Langenaubach",
+        "answer_2": "Sechshelden",
+        "answer_3": "Breitscheid",
+        "answer_4": "Rabenscheid",
+        "right_answer": 1
+    },
+
+    {
+        "question": "Welcher Verein hat die besten Fans?",
+        "answer_1": "Eintracht Frankfurt",
+        "answer_2": "Bayern München",
+        "answer_3": "Borussia Dortmund",
+        "answer_4": "VFL Fellerdilln",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Welcher Verein hat die schlechtesten Fans?",
+        "answer_1": "Eintracht Frankfurt",
+        "answer_2": "Bayern München",
+        "answer_3": "Borussia Dortmund",
+        "answer_4": "VFL Fellerdilln",
+        "right_answer": 4
+    },
+
+    {
+        "question": "Welcher Verein hat spielt nicht in der zweiten DFB Pokalrunde?",
+        "answer_1": "Eintracht Frankfurt",
+        "answer_2": "Bayern München",
+        "answer_3": "Borussia Dortmund",
+        "answer_4": "FC Köln",
+        "right_answer": 1
+    },
+
+];
